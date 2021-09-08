7292c5dfefad8357ef117947fb1ed3d4c0ab4882
Krevil <Krevil.3000@gmail.com>
2021-09-08 13:49:59 +0100

    Update chud_messaging.cpp

--- halo3_hook/source/interface/chud/chud_messaging.cpp
+++ halo3_hook/source/interface/chud/chud_messaging.cpp
@@ -4,7 +4,7 @@
 
 /* ---------- public code */
 
-void __fastcall chud_messaging_post(long user_index, const wchar_t *buffer, long context)
+void __fastcall chud_messaging_post(long long user_index, const wchar_t *buffer, long context)
 {
-	main_get_typed_module_address<decltype(&chud_messaging_post)>(0x2E0C08)(user_index, buffer, context);
+	main_get_typed_module_address<decltype(&chud_messaging_post)>(0x2E0C08)((*main_get_typed_tls_address<long long*>((user_index * 0x28e0) + 0x108) + 0x2000), buffer, context);
 }
